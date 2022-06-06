172a3a0c6eaa35ccc652e24d55b0574d29df7034
Shivasai <38298958+shivasai780@users.noreply.github.com>
2022-06-06 19:11:46 +0530

    Update erc20

--- erc20.sol
+++ erc20.sol
@@ -71,12 +71,4 @@ contract ERC20 is IERC20 {
     }
 }
 
-contract MyToken is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        // Mint 100 tokens to msg.sender
-        // Similar to how
-        // 1 dollar = 100 cents
-        // 1 token = 1 * (10 ** decimals)
-        _mint(msg.sender, 100 * 10**uint(decimals()));
-    }
-}
+
